ebfa062b09b2050786f08b38889617bdc88ff77e
kociumba <79542688+kociumba@users.noreply.github.com>
2024-04-20 22:01:49 +0200

    Initial commit

---
+++ inject.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"syscall"
+
+	clog "github.com/charmbracelet/log"
+	ps "github.com/mitchellh/go-ps"
+	"golang.org/x/sys/windows"
+)
+
+// Inject injects a DLL into a running process.
+//
+// Takes a UserSelection parameter containing the process name and DLL path to inject.
+// Returns an error if any operation fails.
+func Inject(userSlection *UserSelection) error {
+	// var dPath string
+	var _pId int
+	// var pName string
+
+	pName := userSlection.SelectedProc
+	dPath := userSlection.SelectedDll
+	// flag.StringVar(&pName, "process", "", "Process name to inject to")
+	// flag.StringVar(&dPath, "dll", "", "DLL to inject")
+	// flag.Parse()
+	pList, err := ps.Processes()
+	if err != nil {
+		return err
+	}
+	for pI := range pList {
+		process := pList[pI]
+		if process.Executable() == pName {
+			_pId = process.Pid()
+			break
+		}
+	}
+	if _pId == 0 {
+		// clog.Warn("Process not found (from inject.go)")
+		return errors.New("process not found")
+	}
+	pId := uintptr(_pId)
+	if _, err := os.Stat(dPath); errors.Is(err, os.ErrNotExist) {
+		return err
+	}
+	clog.Info(pId)
+
+	kernel32 := windows.NewLazyDLL("kernel32.dll")
+	pHandle, err := windows.OpenProcess(windows.PROCESS_CREATE_THREAD|windows.PROCESS_VM_OPERATION|windows.PROCESS_VM_WRITE|windows.PROCESS_VM_READ|windows.PROCESS_QUERY_INFORMATION, false, uint32(pId))
+	if err != nil {
+		return err
+	}
+	clog.Info("Process opened")
+
+	VirtualAllocEx := kernel32.NewProc("VirtualAllocEx")
+	vAlloc, _, err := VirtualAllocEx.Call(uintptr(pHandle), 0, uintptr(len(dPath)+1), windows.MEM_RESERVE|windows.MEM_COMMIT, windows.PAGE_EXECUTE_READWRITE)
+	if err != nil {
+		clog.Warn(err)
+	}
+	clog.Info("Memory allocated")
+
+	bPtrDpath, err := windows.BytePtrFromString(dPath)
+	if err != nil {
+		return err
+	}
+
+	Zero := uintptr(0)
+	err = windows.WriteProcessMemory(pHandle, vAlloc, bPtrDpath, uintptr(len(dPath)+1), &Zero)
+	if err != nil {
+		return err
+	}
+	clog.Info("Memory written")
+
+	LoadLibAddy, err := syscall.GetProcAddress(syscall.Handle(kernel32.Handle()), "LoadLibraryA")
+	if err != nil {
+		clog.Warn(err)
+	}
+
+	tHandle, _, err := kernel32.NewProc("CreateRemoteThread").Call(uintptr(pHandle), 0, 0, LoadLibAddy, vAlloc, 0, 0)
+	if err != nil {
+		clog.Warn(err)
+	}
+	defer syscall.CloseHandle(syscall.Handle(tHandle))
+	clog.Info("Thread created")
+	clog.Info("DLL injected successfully!")
+
+	return nil
+}
---
+++ injector.go
@@ -0,0 +1,162 @@
+// DLL Injector
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	xwidget "fyne.io/x/fyne/widget"
+	clog "github.com/charmbracelet/log"
+	ps "github.com/mitchellh/go-ps"
+	"github.com/ncruces/zenity"
+)
+
+type UserSelection struct {
+	SelectedProc string
+	SelectedDll  string
+	DllFile      string
+}
+
+func trimFilePath(path string) string {
+	_, fileName := filepath.Split(path)
+	return fileName
+}
+
+func main() {
+	userSelection := &UserSelection{}
+	appIcon := fyne.NewStaticResource(resourceIconPng.StaticName, resourceIconPng.StaticContent)
+
+	// app setup
+	a := app.New()
+	a.Settings().SetTheme(&injectorTheme{})
+	w := a.NewWindow("dll injector")
+	w.Resize(fyne.NewSize(500, 500))
+	w.CenterOnScreen()
+	w.SetIcon(appIcon)
+
+	// create the system tray menu
+	if desk, ok := a.(desktop.App); ok {
+		m := fyne.NewMenu("dll injector",
+
+			// register the show window in tray menu
+			fyne.NewMenuItem("Show", func() {
+				w.Show()
+			}),
+
+			// register the inject current in tray menu
+			fyne.NewMenuItem("Inject w/ current settings", func() {
+				err := Inject(userSelection)
+				if err != nil {
+					zenity.Error(err.Error())
+				} else {
+					zenity.Info("Injected into " + userSelection.SelectedProc + " !")
+				}
+			}),
+		)
+		desk.SetSystemTrayMenu(m)
+		desk.SetSystemTrayIcon(appIcon)
+	}
+
+	w.SetContent(widget.NewLabel("Fyne System Tray"))
+	w.SetCloseIntercept(func() {
+		w.Hide()
+	})
+
+	// get a snapshot of the current running procesess
+	procList, err := ps.Processes()
+	if err != nil {
+		clog.Fatal(err)
+	}
+	// convert it into a list of process names
+	var processNames []string
+	for _, process := range procList {
+		processNames = append(processNames, process.Executable())
+	}
+
+	// register the process selection input
+	procSelect := xwidget.NewCompletionEntry(processNames)
+
+	// update the process name suggestions on the fly
+	procSelect.OnChanged = func(s string) {
+		matchingProcesses := []string{}
+		userSelection.SelectedProc = s // keep this here couse of case sensitivity
+		s = strings.ToLower(s)
+		for _, processName := range processNames {
+			if strings.Contains(strings.ToLower(processName), s) {
+				matchingProcesses = append(matchingProcesses, processName)
+			}
+		}
+		procSelect.SetOptions(matchingProcesses)
+		procSelect.ShowCompletion()
+		clog.Info(s)
+	}
+
+	// register text displays
+	dllDisplay := widget.NewLabelWithStyle("Dll selected: ", fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
+	// errorDisplay := widget.NewLabelWithStyle("", fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
+
+	// create the app layout
+	//
+	//
+	w.SetContent(container.NewVBox(
+
+		// process slection
+		widget.NewLabelWithStyle("Select the process to inject: ", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		procSelect,
+		widget.NewSeparator(),
+
+		// dll selection
+		widget.NewButtonWithIcon("Select dll to load", theme.FolderOpenIcon(), func() {
+			userSelection.SelectedDll, err = zenity.SelectFile(zenity.Filename(os.ExpandEnv("$HOME")), zenity.FileFilter{Patterns: []string{"*.dll"}})
+			userSelection.DllFile = trimFilePath(userSelection.SelectedDll)
+			// if err != nil {
+			// 	zenity.Error(err.Error())
+			// }
+			dllDisplay.SetText("Dll selected: " + userSelection.DllFile)
+		}),
+
+		// display the sleected dll
+		dllDisplay,
+		widget.NewSeparator(),
+
+		// triggers the injection
+		widget.NewButtonWithIcon("Inject", theme.ConfirmIcon(), func() {
+			dialog.NewConfirm(
+				"Inject ?",
+				"Inject "+userSelection.SelectedProc+" with "+userSelection.DllFile+" ?",
+				func(b bool) {
+					if b {
+						err := Inject(userSelection)
+						if err != nil {
+							dialog.NewError(err, w).Show()
+							// errorDisplay.SetText(err.Error())
+							clog.Warn(err)
+						} else {
+							dialog.NewInformation("Success", "Injected into "+userSelection.SelectedProc+" !", w).Show()
+							// errorDisplay.SetText("Injected into " + userSelection.SelectedProc + " !")
+						}
+					}
+				},
+				w,
+			).Show()
+		}),
+		// display injection status
+		// errorDisplay,
+		// quit button
+		widget.NewSeparator(),
+		widget.NewButtonWithIcon("Quit", theme.CancelIcon(), func() {
+			w.Close()
+			a.Quit()
+		}),
+	))
+
+	w.ShowAndRun()
+}
